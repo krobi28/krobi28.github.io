430521f8530bfd6c1b0c4957db679b1a0a057e9f
Karen Robison <karenrobison@Karens-MBP.hsd1.ca.comcast.net>
2020-01-30 21:26:57 -0800

    push

--- lesson04/js/l04.js
+++ lesson04/js/l04.js
@@ -1,8 +1,8 @@
-var newDate = document.lastModified;
-document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString(navigator.language,{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+var newDate = document.currentdate;
+document.getElementById("currentdate").innerHTML=new Date().toLocaleDateString(navigator.language,{weekday:'long',year:'numeric',month:'long',day:'numeric'});
 
 const hambutton = document.querySelector(".ham");
-hambutton.addEventListener("click", toggleMenu, false);
+hambutton.addEventListener("click",toggleMenu,false);
 
 function toggleMenu() {
   document.querySelector(".navigation").classList.toggle("responsive");
