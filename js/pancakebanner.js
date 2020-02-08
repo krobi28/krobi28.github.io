e5ac9094fc3d48d08d27a0b95498c81a4196588f
Karen Robison <karenrobison@Karens-MacBook-Pro.local>
2020-02-07 23:55:39 -0800

    push

--- js/pancakebanner.js
+++ js/pancakebanner.js
@@ -4,9 +4,9 @@ const dayNumber = today.getDay();
 
 const element = document.getElementById ("message");
 
-if (dayNumber == 5) {
-    element.classList.add("showme");
+if (dayNumber ==5) {
+    element.classList.add("showbanner");
 }
 else {
-    element.classList.add("hideme");
+    element.classList.add("hidebanner");
 }
