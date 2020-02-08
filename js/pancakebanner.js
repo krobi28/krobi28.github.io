52ef87f44c1dad76dee54179b9949a1cd024e105
Karen Robison <karenrobison@Karens-MBP.hsd1.ca.comcast.net>
2020-02-08 09:02:15 -0800

    lo5push

--- js/pancakebanner.js
+++ js/pancakebanner.js
@@ -4,7 +4,7 @@ const dayNumber = today.getDay();
 
 const element = document.getElementById ("message");
 
-if (dayNumber ==6) {
+if (dayNumber ==5) {
     element.classList.add("showbanner");
 }
 else {
