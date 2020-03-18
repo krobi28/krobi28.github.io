685b59a12baf804fc3a79b707ed650c6a7b25053
Karen Robison <karenrobison@Karens-MBP.hsd1.ca.comcast.net>
2020-03-18 14:04:43 -0700

    Peer Review Push

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -21,7 +21,6 @@ fetch(apiURLforecast)
     for (let i = 0; i < fivedayforecast.length; i++) {
       document.getElementById(`high${i+1}`).textContent = fivedayforecast[i].main.temp.toFixed(0) + "\xB0";
       document.getElementById(`day${i+1}`).textContent = weekday[(date.getDay() + i + 1) % 7];
-
       document.getElementById(`imagesrc${i+1}`).setAttribute('src', "https://openweathermap.org/img/wn/" + fivedayforecast[i].weather[0].icon + "@2x.png");
       document.getElementById(`imagesrc${i+1}`).setAttribute('alt', fivedayforecast[i].weather[0].description);
 
