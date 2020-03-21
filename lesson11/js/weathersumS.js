20e31b39c89456ba3552cc8c1e734b861c4fcd6f
Karen Robison <karenrobison@Karens-MBP.hsd1.ca.comcast.net>
2020-03-20 17:42:11 -0700

    push

--- lesson11/js/weathersumS.js
+++ lesson11/js/weathersumS.js
@@ -27,7 +27,7 @@ fetch(apiURL)
         if (t<=50 && s>=3) {
             windchill = 35.74 + (0.6215 * t) - (35.75 * (Math.pow(s, 0.16))) + (0.425 * t * (Math.pow(s, 0.16)));
         
-            document.getElementById('windchill').textContent = (windchill).toFixed(0);
+            document.getElementById('windchill').textContent = (windchill).toFixed(0) + " ºF";
         }
         
         else {
