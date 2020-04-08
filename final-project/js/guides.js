3ea85a7b6c40dbe05773988bbc505ba866fb0ff0
Karen Robison <karenrobison@Karens-MBP.hsd1.ca.comcast.net>
2020-04-08 16:03:50 -0700

    push

--- final-project/js/guides.js
+++ final-project/js/guides.js
@@ -1,4 +1,4 @@
-/*const requestURL = 'https://github.com/krobi28/krobi28.github.io/blob/master/final-project/guide.json';
+const requestURL = 'https://github.com/krobi28/krobi28.github.io/blob/master/final-project/guides.json';
 
 
 fetch(requestURL)
@@ -6,46 +6,20 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        const guide = jsonObject['guide.json'];*/
+        const guides = jsonObject['guides'];
 
-let requestURL = 'https://github.com/krobi28/krobi28.github.io/blob/master/final-project/guides.json';
-
-let request = new XMLHttpRequest();
-
-request.open('GET', requestURL);
-
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    const guides = request.response;
-    display(guides);
-}
-
-function display(jsonObject) {
-    const guides = jsonObect['guides'];
-}
-
+  
         for (let i = 0; i < guides.length; i++) {
-            if (guides[i].name == 'Jeff Thompson' || guides[i].name == 'Keith Wilson' || guides[i].name == 'Ben Turner' || guides[i].name == 'Nick Johnson') {
+            let guides = guides[i];
 
-                const card = document.createElement('section');
-                const div = document.createElement('div');
-                const h2 = document.createElement('h2');
-                const name = document.createElement('p');
-                const email = document.createElement('p');
-                const certification = document.createElement('p');
-                const yearsOfExperience = document.createElement('p');
-                const img = document.createElement('img');
-
-                /*let card = document.createElement('section');
+             
                 let div = document.createElement('div');
                 let h2 = document.createElement('h2');
                 let name = document.createElement('p');
                 let email = document.createElement('p');
                 let certification = document.createElement('p');
                 let yearsOfExperience = document.createElement('p');
-                let img = document.createElement('img');*/
+                let img = document.createElement('img');
                 
                
                 h2.textContent = guides[i].name;
@@ -60,20 +34,19 @@ function display(jsonObject) {
 
 
                 div.appendChild(h2);
+                guides.appendChild(div);
+                guides.appendChild(img);
                 div.appendChild(name);
                 div.appendChild(email);
                 div.appendChild(certification);
                 div.appendChild(yearsOfExperience);
 
-                guides.appendChild(div);
-                guides.appendChild(img);
+                
+                
 
 
-                document.querySelector('div.cards').appendChild(card);
+                document.querySelector('div.guides').appendChild(guides);
             }
-        }
+       
 
-   ;
-
-
-    
+    });
