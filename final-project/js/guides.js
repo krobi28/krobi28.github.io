b2ead57808be4f133d65a4d064a6c219b44085d8
Karen Robison <karenrobison@Karens-MBP.hsd1.ca.comcast.net>
2020-04-08 19:05:31 -0700

    push

--- final-project/js/guides.js
+++ final-project/js/guides.js
@@ -2,51 +2,47 @@ const requestURL = 'https://github.com/krobi28/krobi28.github.io/blob/master/fin
 
 
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        const guides = jsonObject['guides'];
 
-  
+
+    .then((data) => data.json())
+    .then((guides) => {
+
+
+
         for (let i = 0; i < guides.length; i++) {
             let guides = guides[i];
 
-             
-                let div = document.createElement('div');
-                let h2 = document.createElement('h2');
-                let name = document.createElement('p');
-                let email = document.createElement('p');
-                let certification = document.createElement('p');
-                let yearsOfExperience = document.createElement('p');
-                let img = document.createElement('img');
-                
-               
-                h2.textContent = guides[i].name;
-                name.textContent = "Name: " + guides[i].name;
-                email.textContent = "e-mail: " + guides[i].email;
-                certification.textContent = "Certification Level: " + guide[i].certification;
-                yearsOfExperience.textContent = "Years of Experience: " + guide[i].yearsOfExperience;
+
+            let div = document.createElement('div');
+            let h2 = document.createElement('h2');
+            let name = document.createElement('p');
+            let email = document.createElement('p');
+            let certification = document.createElement('p');
+            let yearsOfExperience = document.createElement('p');
+            let img = document.createElement('img');
 
 
-                img.setAttribute('src', '/images/' + guides[i].img);
-                img.setAttribute('alt', "picture of: " + h2.textContent);
+            h2.textContent = guides[i].name;
+            name.textContent = "Name: " + guides[i].name;
+            email.textContent = "e-mail: " + guides[i].email;
+            certification.textContent = "Certification Level: " + guide[i].certification;
+            yearsOfExperience.textContent = "Years of Experience: " + guide[i].yearsOfExperience;
 
 
-                div.appendChild(h2);
-                guides.appendChild(div);
-                guides.appendChild(img);
-                div.appendChild(name);
-                div.appendChild(email);
-                div.appendChild(certification);
-                div.appendChild(yearsOfExperience);
-
-                
-                
+            img.setAttribute('src', '/images/' + guides[i].img);
+            img.setAttribute('alt', "Picture of: " + h2.textContent);
 
 
-                document.querySelector('div.guides').appendChild(guides);
-            }
-       
+            div.appendChild(h2);
+            guides.appendChild(div);
+            guides.appendChild(img);
+            div.appendChild(name);
+            div.appendChild(email);
+            div.appendChild(certification);
+            div.appendChild(yearsOfExperience);
+
+            document.querySelector('div.guides').appendChild(guides);
+        }
+
 
     });
